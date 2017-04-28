21ed4e123d2eb77c66ab53a5f08aec95bc6307de
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-28 12:54:47 -0500

    validacion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
 var inputAll=document.getElementsByTagName("input");
-var micel=document.getElementById("celular");
-var prueba=/[0-9]/;
+var enviar=document.getElementById("sign-in");
+enviar.addEventListener("click",function(){
+  if (!(inputAll[6].value==inputAll[7].value)) {
+  alert("las no contraseñas coenciden");
+  }
+
+});
+
+
+
 /*
 if(!prueba.test(micel.value)){
   alert("ingrese solo numeros");
