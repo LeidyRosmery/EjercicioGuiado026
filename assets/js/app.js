ca66bd98989ec178ca609eced558286f798d4d1d
LeidyRosmery <rosm3rita@hotmail.com>
2017-04-28 12:40:00 -0500

    validacion regEx

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+var inputAll=document.getElementsByTagName("input");
+var micel=document.getElementById("celular");
+var prueba=/[0-9]/;
+/*
+if(!prueba.test(micel.value)){
+  alert("ingrese solo numeros");
+}*/
